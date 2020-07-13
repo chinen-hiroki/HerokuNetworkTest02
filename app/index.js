bd5b19998b2265a1c9855dfd64b40d52771cb105
chinen-hiroki <chinen.hiroki.2486@members.co.jp>
2020-07-13 16:31:43 +0900

    .env add

--- app/index.js
+++ app/index.js
@@ -3,7 +3,23 @@
 const express = require('express')
 const router = require('./module/router')
 const app = express()
-const portForDev = 4001
+const portForDev = 4000
+
+const expressSession = require('express-session')
+const RedisStore = require('connect-redis')(expressSession)
+const Redis = require('ioredis')
+
+// redis設定
+const ioredis = new Redis(process.env.REDIS_URL)
+
+// セッション設定
+app.use(expressSession({
+  secret: process.env.SESSION_KEY || 'secret', // @TODO 環境変数に SESSION_KEY を追加する.
+  name: 'XhFEqXpTFR',
+  store: new RedisStore({ client: ioredis, ttl: 1800 }),
+  resave: false,
+  saveUninitialized: false
+}))
 
 // リバースプロキシ下でリモートIPを取得するには必要
 app.set('trust proxy', 1)
